61942f4058048da71058c55987f5a5a591b173b8
白家松 <bjs13cst@gmail.com>
2015-11-15 01:46:05 +0800

    Integer直接继承Varchar类的实现

--- db/type.h
+++ db/type.h
@@ -12,24 +12,12 @@ public:
 	virtual int write(uint*, int&) = 0;
 };
 
-class Integer : public Type {
-public:
-	int value;
-	Integer(int c) : Type(1) {
-		value = c;
-	}
-	int write (uint* b, int& pos) {
-		// pos - nth byte of page b
-	    uint* bb = (uint*)((char*)b + pos);
-	    (*bb) = value;
-	    pos += 4;
-	}
-};
-
 class Varchar : public Type {
 public:
 	string str;
-	Varchar(int len) : Type(len) {}
+	Varchar(string value, int len) : Type(len) {
+		str = value;
+	}
 	int write (uint* b, int& pos) {
 		// pos - nth byte of page b
 	    char* bb = (char*) b;
@@ -37,8 +25,13 @@ public:
 	    for (int i = 0;i < len;i++) {
 	        *(bb+i) = str[i];
 	    }
-	    pos += len;
+	    pos += length;
 	}
+};
+
+class Integer : public Varchar {
+public:
+	Integer (string value, int len) : Varchar(value, len) {}
 };
 
 #endif
