27e0ba85672f29563f18a282525ff851c48bc7f0
xuhan <orangexuhan@163.com>
2015-12-29 14:04:25 +0800

    complete key restriction

--- db/xh_test.cpp
+++ db/xh_test.cpp
@@ -17,23 +17,23 @@ int main(){
 
 	//test table
 	vector<string> n;
-	n.push_back("xuhan");
+	n.push_back("test2_xuhan");
 	n.push_back("bjs");
 	n.push_back("dhy");
 	vector<string> type;
 	type.push_back("int");
 	type.push_back("varchar");
-	type.push_back("varchar");
+	type.push_back("int");
 	vector<string> len;
 	len.push_back("4");
 	len.push_back("4");
-	len.push_back("5");
+	len.push_back("4");
 	vector<bool> notNull;
 	notNull.push_back(0);
 	notNull.push_back(1);
 	notNull.push_back(0);
 	TableCon* tbc = new TableCon();
-	tbc->init(n, type, "xuhan");
+	tbc->init(n, type, "test2_xuhan");
 	tbc->length = len;
 	tbc->notNull = notNull;
 	tbc->checkAttrs.push_back("bjs");
@@ -51,15 +51,15 @@ int main(){
 	vector<string> v;
 	v.push_back("10");
 	v.push_back("\'F\'");
-	v.push_back("\'aa\'");
+	v.push_back("10");
 	vector<string> ab;
 	ab.push_back("8");
 	ab.push_back("\'F\'");
-	ab.push_back("\'ababa\'");
+	ab.push_back("2");
 	vector<string> abc;
 	abc.push_back("7");
 	abc.push_back("\'M\'");
-	abc.push_back("\'babab\'");
+	abc.push_back("3");
 	vector<vector<string> > vv;
 	vv.push_back(v);
 	vv.push_back(ab);
@@ -70,27 +70,32 @@ int main(){
 		tb2->insert(n, vv);
 	tb->showTB(1);
 
+
+	vector<Table*> tt;
+	tt.push_back(tb2);
+
 	// //test update
 	string tableName = "test";
 	CondSql cond;
 	vector<CondItem> set;
 
-	AttrItem item1("", "xuhan");
+	AttrItem item1("", "test2_xuhan");
 	AttrItem item2("", "");
 	Expression expr;
 	expr.value = 10;
 	CondItem conditem("=", item1, item2, expr);
 
-	AttrItem item3("", "xuhan");
+	AttrItem item3("", "test2_xuhan");
 	Expression expr1;
-	expr1.str = "null";
-	CondItem condi("=", item3, item2, expr1);
+	//expr1.value = 8;
+	AttrItem item4("", "dhy");
+	CondItem condi("=", item3, item4, expr1);
 
 	cond.conditions.push_back(conditem);
 	set.push_back(condi);
 
 	cout << endl;
-	tb->update(set, cond);
+	tb->update(set, cond, tt);
 	tb->showTB(1);
 	//cout << tb->conform(cond, 1, 0) << endl << endl;
 	//cout << tb->conform(cond, 1, 1) << endl;
@@ -102,15 +107,15 @@ int main(){
 	vector<string> tables;
 	CondSql cond1;
 
-	AttrItem item11("", "xuhan");
-	AttrItem item22("test2", "xuhan");
+	AttrItem item11("", "test2_xuhan");
+	AttrItem item22("test2", "test2_xuhan");
 	Expression expr11;
 	//expr11.str = "\'%a%b%\'";
 	CondItem conditem1("=", item11, item22, expr11);
 
 	cond1.conditions.push_back(conditem1);
 	AttrItem item12("test2", "dhy");
-	AttrItem item13("", "xuhan");
+	AttrItem item13("", "test2_xuhan");
 	attrs.push_back(item12);
 	attrs.push_back(item13);
 	tables.push_back("test");
@@ -124,8 +129,6 @@ int main(){
 	// attrID.push_back("xuhan");
 	// attrID.push_back("xuhan");
 	// attrID.push_back("xuhan");
-	vector<Table*> tt;
-	tt.push_back(tb2);
-	tb->select(attrs, cond1, op, attrID, tt);
+	tb->select(attrs, cond1, op, attrID, "", tt);
 	//cout << tb->conform(cond1, 1, 0) << endl;
 }
